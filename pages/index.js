b08293bebd7bf482f983890ad124d582bf07dd12
David Rhodes <david@espressocode.tech>
2023-02-26 15:30:03 -0600

    adds mobile responsive home

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,33 @@
+import styled from 'styled-components';
 import { Body } from '../components/Body';
 import { Header } from '../components/Header';
 
+const Container = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Content = styled.div`
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+  padding: 1rem;
+
+  @media (min-width: 768px) {
+    padding: 1rem 4rem;
+  }
+`;
+
 export default function Home() {
   return (
-    <div>
+    <Container>
       <Header />
 
-      <Body />
-    </div>
+      <Content>
+        <Body />
+      </Content>
+    </Container>
   );
 }
