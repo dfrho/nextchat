e82be2e3798dfd53d9da4c4f00b7cd77057e4999
David Rhodes <david@espressocode.tech>
2023-02-26 12:38:28 -0600

    adds tailwinds

--- components/Body.jsx
+++ components/Body.jsx
@@ -23,9 +23,7 @@ export const Body = () => {
       method: 'POST',
       body: JSON.stringify({ newMessage: message, messageHistory }),
     });
-    console.log("🚀 ~ file: Body.jsx:24 ~ onNewMessage ~ response:", response)
     const data = await response.json();
-    console.log("🚀 file: Body.jsx:26 ~ onNewMessage ~ data:", data)
 
     setMessageHistory((prev) => [
       ...prev,
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { Body } from '../components/Body';
 import { Header } from '../components/Header';
+import '../styles/globals.css';
 
 export default function Home() {
   return (
