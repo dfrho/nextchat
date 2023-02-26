0649378155889179a2fd7df549b7d538873f1fb3
David Rhodes <david@espressocode.tech>
2023-02-26 12:48:18 -0600

    adds styled components

--- components/Message.jsx
+++ components/Message.jsx
@@ -1,25 +1,47 @@
-// Message component, renders a single message. Depending on sender, it will be rendered on the left or right side of the screen
-export const Message = ({ message, sender }) => {
-	return (
-		<>
-			<div
-				className={`bg-gradient-to-r from-blue-800 to-blue-900  shadow-2xl text-white rounded-3xl  max-w-[60%] min-w-[4rem] px-4 py-2  break-words
-							${
-								sender === 'me'
-									? 'self-end rounded-tr-none fade-in-right'
-									: 'self-start rounded-tl-none fade-in-left'
-							} `}
-			>
-				{message.split('\n').map((line, i) => {
-					return (
-						<span key={i}>
-							{line}
+import styled from 'styled-components';
 
-							<br />
-						</span>
-					);
-				})}
-			</div>
-		</>
-	);
+// Create a styled div for the message bubble
+const Bubble = styled.div`
+  background: linear-gradient(to right, #0d6efd, #0dcaf0);
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
+  color: white;
+  max-width: 60%;
+  min-width: 4rem;
+  padding: 1rem;
+  border-radius: 1.5rem;
+  break-word: break-word;
+  animation-duration: 0.5s;
+  animation-fill-mode: both;
+  animation-timing-function: cubic-bezier(0.25, 0.46, 0.45, 0.94);
+
+  /* Add different styles depending on sender */
+  ${({ sender }) =>
+    sender === 'me'
+      ? `
+      align-self: flex-end;
+      border-top-right-radius: 0;
+      animation-name: fadeInRight;
+    `
+      : `
+      align-self: flex-start;
+      border-top-left-radius: 0;
+      animation-name: fadeInLeft;
+    `}
+`;
+
+// Message component, renders a single message.
+export const Message = ({ message, sender }) => {
+  return (
+    <>
+      <Bubble sender={sender}>
+        {/* Split the message by line and wrap each line in a span */}
+        {message.split('\n').map((line, i) => (
+          <span key={i}>
+            {line}
+            <br />
+          </span>
+        ))}
+      </Bubble>
+    </>
+  );
 };
