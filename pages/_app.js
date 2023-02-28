e66a20063761f784838d5aa3aa3289db9a7c2804
David Rhodes <david@espressocode.tech>
2023-02-27 18:24:52 -0600

    corrects font

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
   /* Other global styles go here */
 `;
 
